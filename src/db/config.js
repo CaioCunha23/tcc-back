58789e128027ef0315ca7ad0fb6368f0fd62d306
unknown <admin@LICLABTEC-15.adserver.unisantos.br>
2025-05-22 20:09:31 -0300

    ajuste variaveis de ambiente

--- src/db/config.js
+++ src/db/config.js
@@ -12,10 +12,10 @@ export default {
     },
     production: {
         dialect: process.env.DB_DIALECT,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        port: process.env.DB_PORT,
-        host: process.env.DB_HOST
+        username: process.env.DB_USER_PROD,
+        password: process.env.DB_PASSWORD_PROD,
+        database: process.env.DB_NAME_PROD,
+        port: process.env.DB_PORT_PROD,
+        host: process.env.DB_HOST_PROD
     }
 }
